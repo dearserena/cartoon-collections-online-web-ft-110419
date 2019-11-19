cc8a8e3c694b5ca4fed230a14e86024d2c783d0d
serena Lee <dearserenalee@gmail.com>
2019-11-19 03:26:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array) # code an argument here
   newarray = []
   i = 0 
   while i < array.length
-  newarray << array[i].capitlize +
+  newarray << array[i].capitlize + "!"
 end
 
 def long_planeteer_calls# code an argument here
