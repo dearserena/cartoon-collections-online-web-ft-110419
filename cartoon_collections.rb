dd29e7eefd7cb3f201dbbed84940434440fe8013
serena Lee <dearserenalee@gmail.com>
2019-11-19 03:31:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,8 @@ def long_planeteer_calls(array) # code an argument here
   if array.any? {|i| i.lenght > 4}
     return true 
   else return false 
-    i += 1
+    i += 1 
+  end 
 end
 
 def find_the_cheese# code an argument here
